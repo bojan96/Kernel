fd056f41c836c0e5b74097e685f2ca9695689d59
Bojan Malinic <bojan.malinic@outlook.com>
2018-06-28 00:23:09 +0200

    Declare IRQs, define exception handler

--- src/irq.asm
+++ src/irq.asm
@@ -7,8 +7,9 @@ PIC_EOI equ 0x20
 %endmacro
 
 %macro DECLARE_IRQ 1
-irq%1:
-	mov [irq_code], %1
+global irq_handler_%1
+irq_handler_%1:
+	mov dword [irq_code], %1
 	jmp irq_common
 %endmacro
 
@@ -17,7 +18,7 @@ section .data
 irq_code dd 0
 
 section .text
-
+global remap_irq
 remap_irq:
 	OUT 0x20, 0x11
 	OUT 0xa0, 0x11
@@ -36,6 +37,7 @@ irq_common:
 	pushad ; save all GPR
 	push dword [irq_code]
 	call irq_common_high_level
+	pop ecx
 	cmp dword [irq_code], 8  ; if irq code less than 8 do not send EOI to pic slave
 	jl not_pic2
 	OUT 0xa0, PIC_EOI
@@ -46,3 +48,19 @@ not_pic2:
 	popad ; restore GPR
 	iret
 
+DECLARE_IRQ 0
+DECLARE_IRQ 1
+DECLARE_IRQ 2
+DECLARE_IRQ 3
+DECLARE_IRQ 4
+DECLARE_IRQ 5
+DECLARE_IRQ 6
+DECLARE_IRQ 7
+DECLARE_IRQ 8
+DECLARE_IRQ 9
+DECLARE_IRQ 10
+DECLARE_IRQ 11
+DECLARE_IRQ 12
+DECLARE_IRQ 13
+DECLARE_IRQ 14
+DECLARE_IRQ 15
--- src/irq_high_level.c
+++ src/irq_high_level.c
@@ -1,8 +1,13 @@
 
 
+extern void terminal_writestring(const char* data);
 
 void irq_common_high_level(int irq_code)
 {
-	irq_code++;
+	terminal_writestring("Interrupt detected");
 }
 
+void interrupt_handler(int error_code)
+{
+	terminal_writestring("Interrupt");
+}
