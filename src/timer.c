c2b78e13173d4277c356ee5bcbd8ae113004aa68
Bojan Malinic <bojan.malinic@outlook.com>
2018-08-08 21:55:13 +0200

    Change initTimer signature

--- src/timer.c
+++ src/timer.c
@@ -4,12 +4,17 @@
 
 #define CLOCK_RATE 1193182
 // Minimum allowed frequency is 19 HZ, Max 1193182 HZ
-void initTimer(uint16_t frequency)
+int initTimer(int frequency)
 {
+	if(frequency < 19 || frequency > 1193182)
+		return 0;
+	
 	// Setup timer mode 
 	writePortByte(0x43, 0x34);
 	int count = CLOCK_RATE / frequency;
 	writePortByte(0x40, count & 0xFF);
 	writePortByte(0x40, count >> 8);
+	
+	return 1;
 }
 
--- src/timer.h
+++ src/timer.h
@@ -1,4 +1,4 @@
 #pragma once
 #include <stdint.h>
 
-void initTimer(uint16_t frequency);
+int initTimer(int frequency);
