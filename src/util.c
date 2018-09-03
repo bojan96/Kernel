82fd9f34f4d43205c3e438974fd83541e14474ab
Bojan Malinic <bojan.malinic@outlook.com>
2018-09-03 16:08:05 +0200

    Add memcpy, memmove and itoa implementation

--- src/util.c
+++ src/util.c
@@ -1,4 +1,7 @@
 #include "util.h"
+#include <stdint.h>
+#include <stdbool.h>
+
 
 size_t util_strlen(const char* str) 
 {
@@ -7,3 +10,83 @@ size_t util_strlen(const char* str)
 		len++;
 	return len;
 }
+
+void util_memcpy(void* dest, const void* src, size_t num)
+{
+	const uint8_t* srcByte = (uint8_t*)src;
+	uint8_t* destByte = (uint8_t*)dest;
+	
+	while(num-- > 0)
+		*destByte++ = *srcByte++;
+}
+
+
+static void util_copyBackwards(void* dest, const void* src, size_t num)
+{
+	const uint8_t* srcByte = (uint8_t*)src;
+	uint8_t* destByte = (uint8_t*)dest;
+	
+	while(num-- > 0)
+	{
+		destByte[num] = srcByte[num];
+	}
+}
+
+void util_memmove(void* dest, const void* src, size_t num)
+{
+	uintptr_t srcAddress = (uintptr_t)src;
+	uintptr_t destAddress = (uintptr_t)dest;
+	
+	if(srcAddress == destAddress)
+		return;
+	
+	if(srcAddress < destAddress && srcAddress + num >= destAddress)
+	{
+		util_copyBackwards(dest, src, num);	
+	}
+	// memcpy copies forward
+	else
+		util_memcpy(dest, src, num);
+}
+
+
+static char result[12];
+const char* util_itoa(int num)
+{
+	bool isNegative = false;
+	
+	if(num == 0)
+	{
+		result[0] = '0';
+		result[1] = '\0';
+		return result;
+	}
+	else if(num < 0)
+	{
+		num = -num;
+		isNegative = true;
+		result[0] = '-';
+	}
+	
+	// Include '-' if negative
+	int charCount = isNegative ? 1 : 0;
+	int toCount = num;
+	while(toCount != 0)
+	{
+		++charCount;
+		toCount /= 10;
+	}
+	
+	int i = 1;
+	result[charCount] = '\0';
+	while(num != 0)
+	{
+		// 0x30 is '0';
+		result[charCount - i++] = 0x30 + num % 10;
+		num /= 10;
+	}
+	
+	return result;
+}
+
+
--- src/util.h
+++ src/util.h
@@ -1,3 +1,6 @@
 #pragma once 
 #include <stddef.h>
 size_t util_strlen(const char* str);
+void util_memcpy(void* dest, const void* src, size_t num);
+void util_memmove(void* dest, const void* src, size_t num);
+const char* util_itoa(int num);
